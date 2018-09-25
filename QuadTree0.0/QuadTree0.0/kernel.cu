7e3a30a5d57fe37e789b1b0599f64a3fe66261af
dragod812 <sidtastic11@gmail.com>
2018-09-25 19:07:24 +0530

    Changes to Quadtree0.0

--- QuadTree0.0/QuadTree0.0/kernel.cu
+++ QuadTree0.0/QuadTree0.0/kernel.cu
@@ -1,4 +1,3 @@
-
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <thrust/random.h>
@@ -41,7 +40,7 @@ public:
 class Bounding_Box{
     float xMin , xMax, yMin, yMax;
 public:
-	__host__ __device__ Bouding_Box(){
+	__host__ __device__ Bounding_Box(){
 		xMin = 0;
 		yMin = 0;
 		xMax = 1920;
@@ -68,7 +67,7 @@ public:
 		xMax = X;
 		yMax = y;
 	}
-}
+};
 
 class Quadtree_Node{
 	//node index;
@@ -83,13 +82,20 @@ class Quadtree_Node{
 	__host__ __device__ void setIdx(int idx){
 		this->idx = idx;		
 	}
+	__host__ __device__ int getIdx(){
+		return idx;
+	}
 	__host__ __device__ void setBoundingBox(int x, int y, int X, int Y){
 		bb.set(x, y, X, Y);	
+	}
+	__host__ __device__ __forceinline__ Bounding_Box& getBoundingBox(){
+		return bb;
 	}
 	__host__ __device__ void setRange(int s, int e){
 		sIdx = s;
 		eIdx = e;
 	}
+	/*
 	__host__ __device__ __forceinline__ int getSWChildIdx(){
 		return 2*idx + 1;
 	}
@@ -108,15 +114,62 @@ class Quadtree_Node{
 	__host__ __device__ __forceinline__ int getEndIdx(){
 		return eIdx;
 	}
+	*/
 	__host__ __device__ __forceinline__ int numberOfPoints(){
 		return eIdx - sIdx + 1;
 	}
+};
 
-}
+struct Random_generator
+{
+  __host__ __device__ unsigned int hash(unsigned int a)
+  {
+      a = (a+0x7ed55d16) + (a<<12);
+      a = (a^0xc761c23c) ^ (a>>19);
+      a = (a+0x165667b1) + (a<<5);
+      a = (a+0xd3a2646c) ^ (a<<9);
+      a = (a+0xfd7046c5) + (a<<3);
+      a = (a^0xb55a4f09) ^ (a>>16);
+      return a;
+  }
+
+  __host__ __device__ __forceinline__ thrust::tuple<float, float> operator()() 
+  {
+    unsigned seed = hash( blockIdx.x*blockDim.x + threadIdx.x );
+    thrust::default_random_engine rng(seed);
+    thrust::random::uniform_real_distribution<float> distrib;
+    return thrust::make_tuple( distrib(rng), distrib(rng) );
+  }
+};
+
 
 int main()
 {
-    
+
+	//Set Cuda Device
+  	int device_count = 0, device = -1, warp_size = 0;
+  	checkCudaErrors( cudaGetDeviceCount( &device_count ) );
+	for( int i = 0 ; i < device_count ; ++i )
+	{
+		cudaDeviceProp properties;
+		checkCudaErrors( cudaGetDeviceProperties( &properties, i ) );
+		if( properties.major > 3 || ( properties.major == 3 && properties.minor >= 5 ) )
+		{
+		  device = i;
+		  warp_size = properties.warpSize;
+		  std::cout << "Running on GPU " << i << " (" << properties.name << ")" << std::endl;
+		  break;
+		}
+		std::cout << "GPU " << i << " (" << properties.name << ") does not support CUDA Dynamic Parallelism" << std::endl;
+	}
+	if( device == -1 )
+	{
+		//cdpQuadTree requires SM 3.5 or higher to use CUDA Dynamic Parallelism.  Exiting...
+		exit(EXIT_SUCCESS);
+	}
+	cudaSetDevice(device);
+	getchar();
+
     return 0;
 }
 
